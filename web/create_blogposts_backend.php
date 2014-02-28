215d47fc9587f4fda418da460e480a1a457e1e26
Shivam <shivam.mangla.09@gmail.com>
2014-02-28 09:05:43 +0530

    Correct create_blogposts_backend.php to return data in json format

--- web/create_blogposts_backend.php
+++ web/create_blogposts_backend.php
@@ -13,7 +13,7 @@
 
 		// echo "got the info";//.empty($_POST).empty($errors);
 		if(empty($_POST) === false && empty($errors) === true){
-			echo "POSTED";
+			// echo "POSTED";
 			$register_data = array(
 				'title' 			=> 	$_POST['title'],
 				'description' 		=> 	$_POST['description'],
@@ -26,22 +26,28 @@
 			$data = "'" . implode ("', '", $register_data) . "'";
 			
 			$query = "INSERT INTO blogposts ($fields) VALUES ($data)";
-			$result = mysql_query($query) or die($query."<br/><br/>".mysql_error());
+			$res = mysql_query($query) or die($query."<br/><br/>".mysql_error());
 			// echo $result;
 			
 			$result["status"] = 1;
 			$result["message"] = "Blog posted successfully";
 			$result["error"] = $errors;
+			$result = json_encode($result);
+			echo $result;
 		}
 		else if(empty($errors) === false){
 							
 			$result["status"] = -1;
 			$result["message"] = "Blog post unsuccessful";
 			$result["error"] = $errors;
+			$result = json_encode($result);
+			echo $result;
 		}
 	}else {
 		$result["status"] = -1;
 		$result["message"] = "No data received";
 		$result["error"] = "";
+		$result = json_encode($result);
+		echo $result;
 	}
 ?>
